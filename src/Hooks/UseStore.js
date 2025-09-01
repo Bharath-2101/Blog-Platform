a44fce8369cb88f9024fda2dbc48a698d0a7c710
Bharath <bharthkamatham143@gmail.com>
2025-09-01 18:20:38 +0530

    Added a hook for maintinaing the Nav menu click golabally.

---
+++ src/Hooks/UseStore.js
@@ -0,0 +1,10 @@
+"use client";
+import { create } from "zustand";
+
+const useStore = create((set) => ({
+  hamburgerClick: false,
+  changeHamburgerClick: () =>
+    set((state) => ({ hamburgerClick: !state.hamburgerClick })),
+}));
+
+export default useStore;
