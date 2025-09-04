e20f9b09ad385397ecdaea3d441892a8f31ef252
Bharath <bharthkamatham143@gmail.com>
2025-09-04 19:52:51 +0530

    Added all layout of the main page for testing.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import HeroSection from "@/sections/HeroSection/HeroSection";
+import ArticlesSection from "@/sections/ArticlesSection/ArticlesSection";
 
 const page = () => {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <HeroSection />
+      <ArticlesSection />
+      <div className="h-[100dvh]"></div>
+    </div>
+  );
 };
 
 export default page;
