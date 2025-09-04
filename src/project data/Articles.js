214f1a7b14492329cef8c35ed096ee4a933f282e
Bharath <bharthkamatham143@gmail.com>
2025-09-04 19:57:11 +0530

    Dummy Articles for the development.

---
+++ src/project data/Articles.js
@@ -0,0 +1,18 @@
+const Articles = [
+  { title: "React sub-components", year: "2025" },
+  {
+    title: "Using Flow generics to type generic React components",
+    year: "2025",
+  },
+  { title: "Rebuilding Redux with Hooks and Context", year: "2025" },
+  {
+    title: "Asynchronous rendering with ReactAsynchronous rendering with React",
+    year: "2025",
+  },
+  {
+    title: "Getting started with Typescript on Gatsby",
+    year: "2025",
+  },
+];
+
+export { Articles };
